2812d51cb8b380f15e512dca9ed30c89e389ff73
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-08-18 03:54:42 -0600

    Add function to check if a file exists.

--- lib/clamxir.ex
+++ lib/clamxir.ex
@@ -1,18 +1,33 @@
 defmodule Clamxir do
+  defstruct check: false,
+            daemonize: false,
+            error_clamscan_missing: false,
+            error_file_missing: false,
+            error_file_virus: false,
+            fdpass: false,
+            silence_output: false,
+            stream: false
+
   @moduledoc """
   Documentation for Clamxir.
   """
 
   @doc """
-  Hello world.
+  Returns a tuple with the result of the function.
 
   ## Examples
 
-      iex> Clamxir.hello()
-      :world
+      iex> Clamxir.file_exists?("README.md")
+      {:ok, "README.md"}
 
+      iex> Clamxir.file_exists?("NO_FILE.md")
+      {:error, "NO_FILE.md not found."}
   """
-  def hello do
-    :world
+
+  def file_exists?(path) do
+    case File.exists?(path) do
+      true -> {:ok, path}
+      false -> {:error, "#{path} not found."}
+    end
   end
 end
--- test/clamxir_test.exs
+++ test/clamxir_test.exs
@@ -1,8 +1,4 @@
 defmodule ClamxirTest do
   use ExUnit.Case
   doctest Clamxir
-
-  test "greets the world" do
-    assert Clamxir.hello() == :world
-  end
 end
