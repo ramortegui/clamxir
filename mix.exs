92960bb80822adadbc05b9e78b146dbfec29619b
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-08-18 20:59:03 -0600

    Fix inch_ex on mix.

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Clamxir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      [{:inch_ex, only: :docs}]
+      {:inch_ex, only: :docs},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
