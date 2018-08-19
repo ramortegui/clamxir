867d34c8c1e776b38fac84ed55e302d25e492fdd
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-08-18 23:25:47 -0600

    Install clamav-daemon on travis build.

--- mix.exs
+++ mix.exs
@@ -5,8 +5,11 @@ defmodule Clamxir.MixProject do
     [
       app: :clamxir,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.7.1",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      package: package(),
+      description: description(),
       deps: deps()
     ]
   end
@@ -15,6 +18,21 @@ defmodule Clamxir.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp description do
+    "Clamav wrapper."
+  end
+
+  defp package do
+    # These are the default files included in the package
+    [
+      name: :clamxir,
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Ruben Amortegui"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/ramortegui/clamxir"}
     ]
   end
 
